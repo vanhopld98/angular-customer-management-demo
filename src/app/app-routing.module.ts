e6d6fe219430b6323e8a7e4fb572d57bc8aefdf3
vanhopld98 <vanhopld98@gmail.com>
2021-11-19 09:38:01 +0700

    update edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {CustomerListComponent} from './customer/customer-list/customer-list.component';
 import {CustomerCreateComponent} from './customer/customer-create/customer-create.component';
+import {CustomerEditComponent} from './customer/customer-edit/customer-edit.component';
 
 
 const routes: Routes = [
@@ -12,6 +13,10 @@ const routes: Routes = [
   {
     path: 'customers/create',
     component: CustomerCreateComponent
+  },
+  {
+    path: 'customers/edit/:id',
+    component: CustomerEditComponent
   }
 ];
 
--- src/app/customer/customer-edit/customer-edit.component.ts
+++ src/app/customer/customer-edit/customer-edit.component.ts
@@ -1,7 +1,7 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Customer} from '../../customer';
 import {CustomerService} from '../../service/customer.service';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-customer-edit',
@@ -9,12 +9,22 @@ import {Router} from '@angular/router';
   styleUrls: ['./customer-edit.component.css']
 })
 export class CustomerEditComponent implements OnInit {
+  customer: Customer = {};
+  index;
 
   constructor(private customerService: CustomerService,
-              private router: Router) {
+              private activatedRoute: ActivatedRoute) {
+    this.activatedRoute.paramMap.subscribe(paramMap => {
+      this.customer.id = +paramMap.get('id');
+      this.index = this.customerService.getById(this.customer.id);
+    });
   }
+
 
   ngOnInit() {
   }
 
+  submitEdit(formEditCustomer) {
+    this.customerService.update(this.index, formEditCustomer.value);
+  }
 }
