d4170da71026356981b53b5ba481bbf176a45a45
vanhopld98 <vanhopld98@gmail.com>
2021-11-21 11:27:50 +0700

    update create category 100%

--- src/app/category/category-create/category-create.component.ts
+++ src/app/category/category-create/category-create.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CategoryService} from '../../service/category/category.service';
+import {Router} from '@angular/router';
+import {Category} from '../../category';
 
 @Component({
   selector: 'app-category-create',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category-create.component.css']
 })
 export class CategoryCreateComponent implements OnInit {
+  category: Category = {};
 
-  constructor() { }
+  constructor(private categoryService: CategoryService,
+              private router: Router) {
+  }
 
   ngOnInit() {
   }
 
+  submit(formCreateCategory) {
+    this.categoryService.create(formCreateCategory.value).subscribe((data) => {
+      this.category = data;
+      this.router.navigate(['/categories']);
+    });
+  }
 }
